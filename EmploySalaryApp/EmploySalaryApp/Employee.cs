f94846a589417d4322ae88889461878ee0cb474f
sujoysuktoemon <sujoysuktoemon@gmail.com>
2014-09-29 11:42:55 +0600

    apps added

---
+++ EmploySalaryApp/EmploySalaryApp/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmploySalaryApp
+{
+    class Employee
+    {
+        public string name;
+
+        public double salary;
+        public double houseRent=0;
+        public double medicalAlloance;
+
+        public double GetHouseRent()
+        {
+            return (salary*houseRent)/100;
+        }
+        public double GetMedical()
+        {
+            return (salary * medicalAlloance) / 100;
+        }
+
+        public double GetTotal()
+        {
+            return salary + GetHouseRent() + GetMedical();
+        }
+
+    }
+}
---
+++ EmploySalaryApp/EmploySalaryApp/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmploySalaryApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Employee anEmployee=new Employee();
+        private void showMeSalaryButton_Click(object sender, EventArgs e)
+        {
+            anEmployee.name = employeeNameTextBox.Text;
+            anEmployee.salary = Convert.ToDouble(basicAmountTextBox.Text);
+            anEmployee.houseRent = Convert.ToDouble(houseRentTextBox.Text);
+            anEmployee.medicalAlloance = Convert.ToDouble(medicalAllowanceTextBox.Text);
+            double total = anEmployee.GetTotal();
+            MessageBox.Show("your Name is" + anEmployee.name   + " " + "totalsalary is"+ anEmployee.GetTotal());
+        }
+    }
+}
